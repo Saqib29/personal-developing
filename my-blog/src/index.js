95cf95dd641f79a26b058b204158a34533188999
Saqib <saqib29111@gmail.com>
2021-01-22 17:13:30 +0600

    more on css in JSX & design a card

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 
 const todoTitle = 'Call Family'
 const todoDesc = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, et?";
@@ -12,9 +12,11 @@ const currentYear = date.getFullYear();
 ReactDOM.render(
   <div>
     <h1 className="headingStyle">Todo App</h1>
-    <h3>{ todoTitle }</h3>
-    <p>{ todoDesc }</p>
-    <p>{ dateName + "/" + monthNAme + "/" + currentYear }</p>
+    <div className="card">
+      <h3 className="cardTitle">{ todoTitle }</h3>
+      <p className="cardDesc">{ todoDesc }</p>
+      <p className="cardFooter">{ dateName + "/" + monthNAme + "/" + currentYear }</p>
+    </div>
   </div>,
   document.getElementById('root')
 );

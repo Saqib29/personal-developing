da4695fcf3baa75d5aa82d7c9e5b8776a6001994
Saqib <saqib29111@gmail.com>
2021-01-22 15:48:10 +0600

    JFX + dynamic variable

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const todoTitle = 'Call Family'
+const todoDesc = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, et?";
+const date = new Date();
+const dateName = date.getDate();
+const monthNAme = date.getMonth();
+const currentYear = date.getFullYear();
 
 ReactDOM.render(
-  <h1>Hello React App</h1>,
+  <div>
+    <h1>Todo App</h1>
+    <h3>{ todoTitle }</h3>
+    <p>{ todoDesc }</p>
+    <p>{ dateName + "/" + monthNAme + "/" + currentYear }</p>
+  </div>,
   document.getElementById('root')
 );

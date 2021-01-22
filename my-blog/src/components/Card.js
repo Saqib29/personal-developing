7bfbb379ee8a7ba6c695fb30d08add745495eead
Saqib <saqib29111@gmail.com>
2021-01-22 20:15:28 +0600

    props destructuring

--- my-blog/src/components/Card.js
+++ my-blog/src/components/Card.js
@@ -7,6 +7,7 @@ const monthNAme = date.getMonth();
 const currentYear = date.getFullYear();
 
 function Card(props){
+  const { titleText, descText } = props;
     return  <div className="card">
               <h3 className="cardTitle">{ props.titleText }</h3>
               <p className="cardDesc">{ props.descText }</p>

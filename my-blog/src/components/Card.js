79ef44ca93aa476049cad3eb366cd757b64596c4
Saqib <saqib29111@gmail.com>
2021-01-22 17:29:56 +0600

    create & use component

---
+++ my-blog/src/components/Card.js
@@ -0,0 +1,18 @@
+
+const todoTitle = 'Call Family'
+const todoDesc = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, et?";
+const date = new Date();
+const dateName = date.getDate();
+const monthNAme = date.getMonth();
+const currentYear = date.getFullYear();
+
+function Card(){
+    return  <div className="card">
+              <h3 className="cardTitle">{ todoTitle }</h3>
+              <p className="cardDesc">{ todoDesc }</p>
+              <p className="cardFooter">{ dateName + "/" + monthNAme + "/" + currentYear }</p>
+            </div>
+}
+
+
+export default Card;
--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from "./components/Card";
 
-
-const todoTitle = 'Call Family'
-const todoDesc = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, et?";
-const date = new Date();
-const dateName = date.getDate();
-const monthNAme = date.getMonth();
-const currentYear = date.getFullYear();
 
 ReactDOM.render(
   <div>
     <h1 className="headingStyle">Todo App</h1>
-    <div className="card">
-      <h3 className="cardTitle">{ todoTitle }</h3>
-      <p className="cardDesc">{ todoDesc }</p>
-      <p className="cardFooter">{ dateName + "/" + monthNAme + "/" + currentYear }</p>
-    </div>
+    <Card />
+    <Card />
+    <Card />
   </div>,
   document.getElementById('root')
 );

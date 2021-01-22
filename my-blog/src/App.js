58d73114d9a7e7777b4ff1a60599d840b76d0eec
Saqib <saqib29111@gmail.com>
2021-01-22 20:44:15 +0600

    mapping data to components

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -6,9 +6,8 @@ import Data from './data.json';
 function App(){
 
     let items = [];
-    for(let x=0; x<Data.length; x++){
-        items.push(<Card titleText={Data[x].title} descText={Data[x].desc}/>);
-    }
+    
+    items = Data.map((data) => <Card titleText={data.title} descText={data.desc}/>);
 
     return  <div>
                 <h1 className="headingStyle">Todo App</h1>

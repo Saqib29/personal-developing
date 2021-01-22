794c89e8289aafcbcf41caf2eb4ddd348bcce07b
Saqib <saqib29111@gmail.com>
2021-01-22 20:03:50 +0600

    props

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -4,7 +4,9 @@ import Card from "./components/Card";
 function App(){
     return  <div>
                 <h1 className="headingStyle">Todo App</h1>
-                <Card />
+                <Card titleText="Call Mother" descText="this is desc1"/>
+                <Card titleText="Call Father" descText="this is desc2"/>
+                <Card titleText="Call Wife" descText="this is desc3"/>
             </div>
 }
 
--- my-blog/src/components/Card.js
+++ my-blog/src/components/Card.js
@@ -6,10 +6,10 @@ const dateName = date.getDate();
 const monthNAme = date.getMonth();
 const currentYear = date.getFullYear();
 
-function Card(){
+function Card(props){
     return  <div className="card">
-              <h3 className="cardTitle">{ todoTitle }</h3>
-              <p className="cardDesc">{ todoDesc }</p>
+              <h3 className="cardTitle">{ props.titleText }</h3>
+              <p className="cardDesc">{ props.descText }</p>
               <p className="cardFooter">{ dateName + "/" + monthNAme + "/" + currentYear }</p>
             </div>
 }

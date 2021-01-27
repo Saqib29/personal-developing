ab1a599ab54c825748f76888d142b9dce53ee7cd
Saqib <saqib29111@gmail.com>
2021-01-27 20:39:54 +0600

    Event Handler Bindin : if doesn't use arrow function axcept normal function then shoul use binding

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import CONDITIONAL_RENDERING from './CONDITIONAL_RENDERING/index'
-import EVENT_HANDLER_CLASS from './EVENT_HANDLER_CLASS/index'
+import EVENT_BINDING from './EVENT_BINDING/index'
 
 export default function App() {
     return (
         <div>
-            <EVENT_HANDLER_CLASS />
+            <EVENT_BINDING />
         </div>
     )
 }

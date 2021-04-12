33211b253f59762af0c71743e1138af7acce5c54
saqib <saqib29111@gmail.com>
2021-04-12 21:28:37 +0600

    session in flask

--- flask_basic/app.py
+++ flask_basic/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, redirect, render_template, url_for, request
+from flask import Flask, redirect, render_template, url_for, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+# session needs a secrete key
+app.secret_key = "secrete_key"
+# time can be set for session as minutes/days/seconds
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
@@ -9,17 +14,31 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        # if true then timeset will work
+        session.permanent_session_lifetime = True
         name = request.form["nm"]
-        return redirect(url_for("user", usr=name))
+        session["name"] = name
+        return redirect(url_for("user", usr=session["name"]))
     else:
+        if "name" in session:
+            return redirect(url_for("user"))
+
         return render_template("login.html")
 
 
-@app.route("/<usr>")
-def user(usr):
-    return render_template("features.html", name=usr)
+@app.route("/user")
+def user():
+    if "name" in session:
+        return render_template("features.html", name=session["name"])
+    else:
+        return redirect(url_for("login"))
 
 
+@app.route("/logout")
+def logout():
+    # removing/destroying session
+    session.pop("name", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)

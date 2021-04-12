1bfa6c6b77b59013099c08867ebb7624424c6aa1
saqib <saqib29111@gmail.com>
2021-04-12 21:58:44 +0600

    flash messages in flask

--- flask_basic/app.py
+++ flask_basic/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, url_for, request, session
+from flask import Flask, redirect, render_template, url_for, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -18,9 +18,11 @@ def login():
         session.permanent_session_lifetime = True
         name = request.form["nm"]
         session["name"] = name
+        flash("Login successful!")
         return redirect(url_for("user", usr=session["name"]))
     else:
         if "name" in session:
+            flash("Already logged IN!")
             return redirect(url_for("user"))
 
         return render_template("login.html")
@@ -31,13 +33,17 @@ def user():
     if "name" in session:
         return render_template("features.html", name=session["name"])
     else:
+        flash("You are not logged in!")
         return redirect(url_for("login"))
 
 
 @app.route("/logout")
 def logout():
+    flash(f"You have been logged out", "info")
+
     # removing/destroying session
     session.pop("name", None)
+    # for flash messages
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
